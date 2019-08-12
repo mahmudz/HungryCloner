d7464232c98cc37aad39817c17b672a0f8496736
mahmudz <mahmudbappy.pri@gmail.com>
2019-08-12 07:05:30 +0600

    All repo loader function added.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QMessageBox>
+#include <QNetworkReply>
+#include <QJsonDocument>
+#include <QJsonObject>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
     ui->getReposBTN->setEnabled(false);
     ui->downloadSelectedBTN->setEnabled(false);
+    ui->allCheck->setEnabled(false);
 }
 
 MainWindow::~MainWindow()
@@ -27,13 +32,63 @@ void MainWindow::on_inName_textChanged(const QString &data)
 
 void MainWindow::on_getReposBTN_clicked()
 {
-    QString username =  ui->inName->text();
+    QString username = ui->inName->text();
+    QString reposURL;
 
-    QString subStr(":user");
-    if (ui->inType->currentIndex() == 1) {
-        QString userReposURL = "https://api.github.com/users/" + username + "/repos";
+    if (ui->inType->currentIndex() == 0) {
+        reposURL = "https://api.github.com/users/" + username + "/repos";
     }else {
-        QString orgReposURL = "https://api.github.com/orgs/" + username + "/repos";
+        reposURL = "https://api.github.com/orgs/" + username + "/repos";
     }
 
+    ui->console->append("Fetching repo list...");
+
+    QNetworkRequest request((QUrl(reposURL)));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+
+    QNetworkReply *reply = manager.get(request);
+
+    ui->console->append("Sending request...");
+
+    while (!reply->isFinished()) {
+        qApp->processEvents();
+    }
+
+
+    QByteArray response_data = reply->readAll();
+    QJsonDocument repos = QJsonDocument::fromJson(response_data);
+
+    reply->close();
+    repoArray = repos.array();
+
+    for(const QJsonValue& val: repoArray) {
+        QJsonObject loopObj = val.toObject();
+
+        QString repoName = loopObj["name"].toString();
+
+        QTableWidgetItem *repoNameItem = new QTableWidgetItem();
+        repoNameItem->setCheckState(Qt::Checked);
+        repoNameItem->setText(repoName);
+
+        int rowCount = ui->repoTable->rowCount();
+        ui->repoTable->insertRow(rowCount);
+
+        ui->repoTable->setItem(rowCount, 0, repoNameItem);
+
+    }
+    ui->allCheck->setEnabled(true);
+    ui->downloadSelectedBTN->setEnabled(true);
+    ui->allCheck->setCheckState(Qt::Checked);
+    ui->console->append("Data Loaded.");
+
 }
+
+void MainWindow::on_actionAbout_triggered()
+{
+    QMessageBox about;
+    about.setText("About");
+    about.exec();
+}
+
+
